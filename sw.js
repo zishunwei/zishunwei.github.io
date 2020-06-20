1301061602831ecafbe010111ba0b20e76d8468c
zishun <uceszwe@ucl.ac.uk>
2020-06-21 00:17:19 +0100

    update

--- sw.js
+++ sw.js
@@ -8,3 +8,9 @@ self.addEventListener('activate', function (event) {
         return self.clients.claim()
     }
 );
+
+self.addEventListener('fetch', function(event) {
+    console.log('[Service Worker] Fetching something ....', event);
+    event.respondWith(fetch(event.request));
+});
+
