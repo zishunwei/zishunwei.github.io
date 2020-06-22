76a2f23c193f1c82416469f39f03ff325140731a
zishun <uceszwe@ucl.ac.uk>
2020-06-22 13:16:11 +0100

    add image

--- sw.js
+++ sw.js
@@ -5,9 +5,13 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cacheName)
             .then(cache => cache.addAll([
-                '/src/js/basicMap.js',
-                '/index.html',
-                'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+                './src/js/basicMap.js',
+                './index.html',
+                './src/images/leaflet-logo.png',
+                'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+                'https://unpkg.com/leaflet@1.1.0/dist/leaflet.css',
+                'https://unpkg.com/leaflet@1.1.0/dist/leaflet.js',
+                'https://code.jquery.com/jquery-3.4.1.min.js'
             ]))
     );
 });
