60a0906bc048553ad3326a2c3a980997b09ac3db
zishun <uceszwe@ucl.ac.uk>
2020-06-28 15:20:42 +0100

    update

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ const cacheFiles = [
     'images/leaflet-logo.png',
     'css/sb-admin-2.css',
     'js/sb-admin-2.js',
+    '/manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css',
     'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
     'https://code.jquery.com/jquery-3.4.1.min.js',
